b8134fbdaf893d605671ccd62d713d84ec695c08
chrisworkalx <857717575@qq.com>
2023-05-17 18:04:31 +0800

    chore: add react@17 test setState api

--- react系列/react-setState/src/setState.jsx
+++ react系列/react-setState/src/setState.jsx
@@ -6,6 +6,58 @@ export default class SetStateComp extends Component {
     num: 1
   };
 
+  renderButtonGrounps = () => {
+    return (
+      <div style={{ display: 'flex', gap: 10 }}>
+        <button onClick={this.handleClick}>react合成事件触发</button>
+        <button ref={(e) => (this.btnRef = e)}>原生点击事件</button>
+      </div>
+    );
+  };
+
+  handleClick = () => {
+    this.setState(
+      {
+        num: this.state.num + 1
+      },
+      () => {
+        console.log(this.state.num, '第1次'); //7
+      }
+    );
+    this.setState(
+      {
+        num: this.state.num + 1
+      },
+      () => {
+        console.log(this.state.num, '第2次'); //7
+      }
+    );
+    this.setState(
+      {
+        num: this.state.num + 3
+      },
+      () => {
+        console.log(this.state.num, '第3次'); //7
+      }
+    );
+
+    console.log(this.state.num); // react版本 18.2 输出依然是4
+  };
+
+  nativeHandleClick = () => {
+    this.setState({
+      num: this.state.num + 1
+    });
+    this.setState({
+      num: this.state.num + 1
+    });
+    this.setState({
+      num: this.state.num + 3
+    });
+
+    console.log(this.state.num); // react版本 18.2 输出依然是4
+  };
+
   componentDidMount() {
     // this.setState({
     //   num: this.state.num + 1
@@ -14,21 +66,29 @@ export default class SetStateComp extends Component {
     //   num: this.state.num + 1
     // });
     // this.setState({
-    //   num: this.state.num + 1
+    //   num: this.state.num + 3
     // });
-    // setTimeout(() => {
-    //   this.setState({
-    //     num: this.state.num + 1
-    //   });
-    //   this.setState({
-    //     num: this.state.num + 1
-    //   });
-    //   this.setState({
-    //     num: this.state.num + 1
-    //   });
-    //   console.log(this.state.num);
-    // });
-    //输出结果都是this.state.num ---> 1
+
+    // 放在setTimeout结果同上面一样
+    setTimeout(() => {
+      this.setState({
+        num: this.state.num + 1
+      });
+      this.setState({
+        num: this.state.num + 1
+      });
+      this.setState({
+        num: this.state.num + 3
+      });
+      console.log(this.state.num);
+    });
+    // console.log(this.state.num); //react版本 18.2 输出结果都是this.state.num ---> 1
+
+    this.btnRef.addEventListener('click', this.nativeHandleClick, false);
+  }
+
+  componentWillUnmount() {
+    this.btnRef.removeEventListener('click', this.nativeHandleClick, false);
   }
 
   render() {
@@ -37,6 +97,7 @@ export default class SetStateComp extends Component {
       <div>
         {/* 页面显示2 */}
         <h1>{num}</h1>
+        {this.renderButtonGrounps()}
       </div>
     );
   }
