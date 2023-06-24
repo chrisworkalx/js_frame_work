71e3b4eb7edf984c2b7c7f6ffa987ed1f8bd8c0a
chrisworkalx <857717575@qq.com>
2023-06-24 11:46:45 +0800

    fix: 优化数字转中文

--- js/算法题/数字转汉字.js
+++ js/算法题/数字转汉字.js
@@ -1,16 +1,37 @@
 /**
  * @description:  写出一个函数trans，将数字转换成汉语的输出，输入为不超过10000亿的数字
- *  trans(123456) —— 十二万三千四百五十六
-    trans（100010001）—— 一亿零一万零一
- * @param {*} money 是数字
+ *  trans(123456) —— 十二万三千四百五十六元
+ *  trans(123456.12) —— 十二万三千四百五十六元一角二分
+ *  trans(123456.01) —— 十二万三千四百五十六元一分
+    trans（100010001）—— 一亿零一万零一元
+ * @param {*} money 是数字 
+ * @param {*} isRounding 是否需要四舍五入 默认false 表示截取字符串
  * @return {*}
  */
 
-function trans(money) {
-  let chineseCurrencyName = '';
-  if (!money && typeof money === 'number') {
+function trans(money, isRounding = false) {
+  //TODO: 这里有个问题是js数字精度问题 js最大保存数字是16位
+  let intChineseCurrencyName = '';
+  let floatChineseCurrencyName = '';
+  if (!money && typeof money !== 'number') {
     throw new Error('不能为空');
   }
+
+  //小数点后面的金额
+  const chinesDoteUnit = ['角', '分', '厘', '毫', '微', '纳', '皮'];
+
+  /**
+   * @description:
+   * @param {*} n 默认字符串
+   * @return {*} 返回字符串
+   */
+  const substrFloatNumber = (n) => {
+    let chinesDoteUnitLen = chinesDoteUnit.length;
+    if (n.length > chinesDoteUnitLen && isRounding) {
+      return (Number('0.' + n).toFixed(chinesDoteUnitLen) + '').split('.')[1];
+    }
+    return n.substr(0, chinesDoteUnitLen);
+  };
 
   const chineseUpperCurrency = [
     '',
@@ -49,40 +70,65 @@ function trans(money) {
     new Map()
   );
 
-  const moneyList = [...money.toString()];
+  const moneyToString = money + '';
 
+  const splitMoneyByDot = moneyToString.split('.');
+
+  // 整数部分
+  const integerPartAmount = splitMoneyByDot[0] ? [...splitMoneyByDot[0]] : '';
+
+  //小数部分
+  const floatPartAmount = splitMoneyByDot[1]
+    ? splitMoneyByDot[1].length > chinesDoteUnit.length
+      ? [...substrFloatNumber(splitMoneyByDot[1])]
+      : [...splitMoneyByDot[1]]
+    : '';
+
+  //整数部分金额单位转换
   const interceptChineseUpperCurrency = chineseUpperCurrency
-    .slice(0, moneyList.length)
+    .slice(0, integerPartAmount.length)
     .reverse();
 
-  moneyList.forEach((m, index) => {
-    let n = mapNumberToChineseNames.get(m);
-    let unit = n === '零' ? '' : interceptChineseUpperCurrency[index];
+  if (integerPartAmount) {
+    integerPartAmount.forEach((m, index) => {
+      let n = mapNumberToChineseNames.get(m);
+      let unit = n === '零' ? '' : interceptChineseUpperCurrency[index];
 
-    if (
-      /[十百千万](万|亿)/g.test(chineseCurrencyName) &&
-      /[十百千万]?(万|亿)/.test(unit)
-    ) {
-      // console.log('unit', unit);
-      // console.log('chineseCurrencyName', chineseCurrencyName);
-      chineseCurrencyName = chineseCurrencyName.replace(
-        /(?<=[十百千万])(万|亿)/g,
-        ''
-      );
-    }
-    chineseCurrencyName += n + unit;
-  });
+      if (
+        /[十百千万](万|亿)/g.test(intChineseCurrencyName) &&
+        /[十百千万]?(万|亿)/.test(unit)
+      ) {
+        // console.log('unit', unit);
+        // console.log('intChineseCurrencyName', intChineseCurrencyName);
+        intChineseCurrencyName = intChineseCurrencyName.replace(
+          /(?<=[十百千万])(万|亿)/g,
+          ''
+        );
+      }
+      intChineseCurrencyName += n + unit;
+    });
 
-  // return chineseCurrencyName;
-  // .replace(/^(一)([十].*?)$/, '$2')
-  // .replace(/个/g, '')
-  // // .replace(/零[百十千万亿](?!一)/g, '')
-  // .replace(/(?<=零)[百十千](万|亿)$/g, '')
-  // .replace(/(.*)零$/, '$1')
+    intChineseCurrencyName = intChineseCurrencyName
+      .replace(/^(一)([十].*?)$/, '$2')
+      .replace(/(?<!零)(零)+(?!零)/g, '零')
+      .replace(/零$/, '');
+  }
 
-  return chineseCurrencyName
-    .replace(/^(一)([十].*?)$/, '$2')
-    .replace(/(?<!零)(零)+(?!零)/g, '零');
+  if (floatPartAmount) {
+    floatPartAmount.forEach((f, index) => {
+      let floatNum = mapNumberToChineseNames.get(f);
+      let floatUnit = floatNum === '零' ? '' : chinesDoteUnit[index];
+      floatChineseCurrencyName += floatNum + floatUnit;
+    });
+
+    floatChineseCurrencyName = floatChineseCurrencyName.replace(/零/g, '');
+  }
+
+  return (
+    intChineseCurrencyName +
+    (intChineseCurrencyName ? '元' : '') +
+    floatChineseCurrencyName
+  );
 }
 
 // const initMoney = 1203111123056;
@@ -96,8 +142,13 @@ function trans(money) {
 // const initMoney = 10;
 // const initMoney = 1;
 // const initMoney = 200113;
-// const initMoney = 12345601001;
+const initMoney = 12305601001.12345678;
+// const initMoney = 1230561.12345678;
 // const initMoney = 123456;
-const initMoney = 100010001;
+// const initMoney = 100010001.123;
+// const initMoney = 100010001.010207;
+// const initMoney = 100010001.002007;
+// const initMoney = 100010001.01020308;
+// const initMoney = 1001001.012345891;
 
-console.log('trans(initMoney)', trans(initMoney));
+console.log('trans(initMoney)', trans(initMoney, true));
