d104309ae9a11fcfc6083a5aff2238380f763788
chrisworkalx <857717575@qq.com>
2023-12-27 15:53:14 +0800

    feat: 面试demo

--- vue2-advanced/src/views/BaseJsxSlot/JsxSlot.vue
+++ vue2-advanced/src/views/BaseJsxSlot/JsxSlot.vue
@@ -8,13 +8,16 @@ export default {
     return {
       user: {
         name: 'chrisworkalx'
-      }
+      },
+      tip: `消息提示zdsakjhdskadjhlksajdlksjalkdjslakdjslakjdls jsakhdjksahdsak季后赛看到后就开始疯狂解释
+      的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案消息提示的文案`
     };
   },
   render() {
     return (
       <div>
         <h1 class="bg-green">主要介绍关于jsx语法中插槽转换问题</h1>
+        <el-alert type="info" show-icon title={this.tip} closable={false} />
         {this.$scopedSlots.default()}
         {this.$scopedSlots.header({ user: this.user })}
         {this.$scopedSlots.footer()}
