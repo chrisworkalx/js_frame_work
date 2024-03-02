408fd660e5ca30f89337e62895984bf7b969ba64
chrisworkalx <857717575@qq.com>
2024-03-02 21:27:11 +0800

    upd: interview and others

--- js/正则/test1.js
+++ js/正则/test1.js
@@ -14,3 +14,17 @@ console.log('eval(s)', eval(s));
 const pathUrl = 'https://www.google.com/api/v1?username=lisi'.match(reg2);
 
 console.log('pathUrl', pathUrl);
+
+const n = 20240301;
+const tReg = /(\d{4})(\d{4})/g;
+
+const ff = n.toString().replace(tReg, function (a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  return `${b}-${c}`;
+});
+
+console.log('ff', ff);
+
+const digitalReg = /\B(?=(\d{3})+(?!\d))/g; //表示千分位加逗号处理
